c6bf57c3166ee81cd2785526cb27d0b3c0113961
Roland Yonaba <roland.yonaba@gmail.com>
2012-12-19 11:39:22 +0000

    Pushed conf file

---
+++ conf.lua
@@ -0,0 +1,40 @@
+--[[
+Copyright (c) 2012 Roland Yonaba
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--]]
+
+function love.conf(t)
+  t.title = "Physics-based movement & Jump (Demo)"
+  t.author = "Roland_Yonaba"
+  t.url = "https://github.com/Yonaba"
+  t.version = "0.8.0"
+  t.console = true
+  t.screen.width = 800
+  t.screen.height = 600
+  t.screen.fullscreen = false
+  t.screen.vsync = false
+  t.screen.fsaa = 0
+  t.modules.joystick = false
+  t.modules.audio = false
+  t.modules.mouse = false
+  t.modules.sound = false
+  t.modules.physics = false 
+end
